2bd6fef1b64a22fdc26efdaae27d4c606ff57746
QofJ <111940839+QofJ@users.noreply.github.com>
2023-11-18 18:00:32 +0800

    P23 分布式组件-SpringCloud Alibaba-Nacos配置中心-简单示例

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,11 +27,30 @@ import com.atguigu.common.utils.R;
  * @email medizana@gmail.com
  * @date 2023-11-17 20:17:54
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test() {
+        return R.ok().put("name", name).put("age", age);
+    }
+
+    @RequestMapping("/member/list")
+    public R memberCoupons(){
+    	CouponEntity couponEntity = new CouponEntity();
+    	couponEntity.setCouponName("满100减10");
+    	return R.ok().put("coupons", Arrays.asList(couponEntity));
+    }
 
     /**
      * 列表
