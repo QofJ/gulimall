828bd764e65570b4a85d8a401da72291eecbef7a
QofJ <111940839+QofJ@users.noreply.github.com>
2023-12-18 20:30:03 +0800

    P74 商品服务-API-属性分组-分组修改&级联选择器回显

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -2,19 +2,33 @@ package com.atguigu.gulimall.product;
 
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
+import com.atguigu.gulimall.product.service.CategoryService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = GulimallProductApplication.class)
 public class GulimallProductApplicationTests {
 
 	@Autowired
 	BrandService brandService;
+
+	@Autowired
+	CategoryService categoryService;
+
+	@Test
+	public void testFindPath() {
+		Long[] catelogPath = categoryService.findCatelogPath(225L);
+		log.info("完整路径：{}", Arrays.asList(catelogPath));
+	}
 
 	@Test
 	public void contextLoads() {
